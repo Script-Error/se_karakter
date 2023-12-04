c9706b8aa6d3ce3550077da75f3226933f5dfa48
reyyghi <reyhafizh07@gmail.com>
2023-12-04 11:37:11 +0700

    create manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version "cerulean"
+game "gta5"
+author "Script Error Team"
+lua54 "yes"
+
+shared_scripts {
+    "@ox_lib/init.lua"
+}
+
+client_scripts {
+    "client/cl_*.lua"
+}
+
+server_scripts {
+    "@oxmysql/lib/MySQL.lua",
+    "server/sv_*.lua"
+}
