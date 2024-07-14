b484b3c9936e2f5a3ea4b7e03695ce7581a543c4
reyyghi <reyhafizh07@gmail.com>
2024-07-14 21:31:48 +0700

    update pekerjaan

---
+++ data/pekerjaan.lua
@@ -0,0 +1,28 @@
+return {
+    pengangguran = {
+        label = 'Pengangguran',
+        garade = {
+            [0] = {
+                label = 'Wes Mbuh',
+                gajih = 0
+            }
+        }
+    },
+    polisi = {
+        label = "Polisi Tidur",
+        grade = {
+            [0] = {
+                label = "Pangkat 1",
+                gajih = 5
+            },
+            [1] = {
+                label = "Pangkat 2",
+                gajih = 10
+            },
+            [2] = {
+                label = "Pangkat 3",
+                gajih = 15
+            }
+        }
+    }
+}
